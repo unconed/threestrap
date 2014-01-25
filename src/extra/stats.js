f9601ccd4ead77b2ae0dd3830ad09af3434ee3d5
Steven Wittens <steven@acko.net>
2014-01-25 05:53:40 -0800

    dead code

--- src/extra/stats.js
+++ src/extra/stats.js
@@ -7,9 +7,6 @@ THREE.Bootstrap.registerPlugin('stats', {
     var stats = this.stats = new THREE.Stats();
     var style = stats.domElement.style;
 
-    this.begin = stats.begin.bind(stats);
-    this.end = stats.end.bind(stats);
-
     style.position = 'absolute';
     style.top = style.left = 0;
     document.body.appendChild(stats.domElement);
