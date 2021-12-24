0be93b404d0f15ce10f512cb1c31f1a884887a08
Christopher Chudzicki <cchudzicki@Christophers-MacBook-Pro.local>
2021-12-24 17:27:19 -0500

    update time.spec to jest

--- test/core/time.spec.js
+++ test/core/time.spec.js
@@ -1,17 +1,16 @@
-describe("time", function () {
-
+describe('time', function () {
   function stall(val, delay) {
     delay = delay || 0;
-    var k, i = 0;
-    while ((+new Date() / 1000) <= val + delay) {
+    var k,
+      i = 0;
+    while (+new Date() / 1000 <= val + delay) {
       k = ++i * ++i * ++i * ++i * ++i;
     }
   }
 
-  it("installs time values", function () {
-
+  it('installs time values', () => {
     var options = {
-      plugins: ['bind','time'],
+      plugins: ['bind', 'time'],
     };
 
     var three = new THREE.Bootstrap(options);
@@ -28,17 +27,16 @@ describe("time", function () {
     three.destroy();
   });
 
-  it("measures delta / fps correctly", function (cb) {
-
+  it('measures delta / fps correctly', () => {
     var pre, update, render, post, three;
 
     var options = {
-      plugins: ['bind','time'],
+      plugins: ['bind', 'time'],
     };
 
     var three = new THREE.Bootstrap(options);
     var fps = 60;
-    var delta = 1/fps;
+    var delta = 1 / fps;
     var frames = 5;
 
     three.trigger({ type: 'pre' });
@@ -64,29 +62,27 @@ describe("time", function () {
     expect(three.Time.average).toBeGreaterThan(0);
     expect(three.Time.fps).toBeGreaterThan(0);
 
-    expect(Math.abs(three.Time.delta - delta)).toBeLessThan(.005);
+    expect(Math.abs(three.Time.delta - delta)).toBeLessThan(0.005);
     expect(Math.abs(three.Time.fps - fps)).toBeLessThan(5);
-    expect(Math.abs(1/three.Time.average - fps)).toBeLessThan(5);
+    expect(Math.abs(1 / three.Time.average - fps)).toBeLessThan(5);
 
     three.destroy();
-
   });
 
-  it("clock runs at half speed", function (cb) {
-
+  it('clock runs at half speed', () => {
     var pre, update, render, post, three;
 
-    var RATIO = 1/2;
+    var RATIO = 1 / 2;
 
     var options = {
       plugins: ['bind', 'time'],
-      time: { speed: RATIO }
+      time: { speed: RATIO },
     };
 
     var three = new THREE.Bootstrap(options);
     var frames = 5;
     var fps = 60;
-    var delta = 1/fps;
+    var delta = 1 / fps;
 
     three.trigger({ type: 'pre' });
 
@@ -106,30 +102,28 @@ describe("time", function () {
     expect(clockTime).toBeGreaterThan(0);
 
     var ratio = clockTime / realTime / RATIO;
-    var diff = 1.0 - Math.min(ratio, 1/ratio);
+    var diff = 1.0 - Math.min(ratio, 1 / ratio);
     expect(diff).toBeLessThan(0.05);
 
     expect(Math.abs(1.0 / three.Time.step - fps / RATIO)).toBeLessThan(5);
 
     three.destroy();
-
   });
 
-  it("clock waits N frames then starts from 0", function (cb) {
-
+  it('clock waits N frames then starts from 0', () => {
     var pre, update, render, post, three;
 
     var delay = 5;
 
     var options = {
       plugins: ['bind', 'time'],
-      time: { warmup: delay }
+      time: { warmup: delay },
     };
 
     var three = new THREE.Bootstrap(options);
     var frames = delay;
     var fps = 60;
-    var delta = 1/fps;
+    var delta = 1 / fps;
 
     three.trigger({ type: 'pre' });
 
@@ -153,24 +147,22 @@ describe("time", function () {
     expect(clockTime).toBeLessThan(delta * 2);
 
     three.destroy();
-
   });
 
-  it("clock ignores frames longer than timeout", function (cb) {
-
+  it('clock ignores frames longer than timeout', () => {
     var pre, update, render, post, three;
 
-    var delay = 5/60;
+    var delay = 5 / 60;
 
     var options = {
       plugins: ['bind', 'time'],
-      time: { timeout: delay }
+      time: { timeout: delay },
     };
 
     var three = new THREE.Bootstrap(options);
     var frames = 3;
     var fps = 60;
-    var delta = 1/fps;
+    var delta = 1 / fps;
 
     for (var i = 0; i < frames; ++i) {
       stall(three.Time.now, delta);
@@ -195,7 +187,5 @@ describe("time", function () {
     expect(clockTime - start).toBeLessThan(delta * 2);
 
     three.destroy();
-
   });
-
 });
