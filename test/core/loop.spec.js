b1e562778dc873e60eed4d37a1335ba5a538ffc8
Christopher Chudzicki <cchudzicki@Christophers-MacBook-Pro.local>
2021-12-24 16:52:03 -0500

    update loop.spec to jest

--- test/core/loop.spec.js
+++ test/core/loop.spec.js
@@ -1,7 +1,5 @@
-describe("loop", function () {
-
-  it("installs start/stop methods", function () {
-
+describe('loop', function () {
+  it('installs start/stop methods', function () {
     var options = {
       plugins: ['loop'],
       loop: {
@@ -15,11 +13,9 @@ describe("loop", function () {
     expect(three.Loop.stop.call).toBeTruthy();
 
     three.destroy();
-
   });
 
-  it("starts and stops", function (cb) {
-
+  it('starts and stops', function () {
     var options = {
       plugins: ['loop'],
       loop: {
@@ -62,77 +58,49 @@ describe("loop", function () {
     expect(stopped).toBe(true);
 
     three.destroy();
-
   });
 
-  it("loops correctly", function (cb) {
+  it('loops correctly', async () => {
+    let pre, update, render, post, three;
+    let count = 0;
 
-    var pre, update, render, post, three;
+    const options = {
+      init: false,
+      plugins: ['bind', 'loop'],
+    };
 
-    function stall(val) {
-      var k, i = 0;
-      var delay = 10;
-      while (+new Date() <= val + delay) {
-        k = ++i * ++i * ++i * ++i * ++i;
-      }
-    }
+    three = new THREE.Bootstrap(options);
 
-    runs(function () {
-      var options = {
-        init: false,
-        plugins: ['bind','loop'],
-      };
-
-      three = new THREE.Bootstrap(options);
-
-      three.on('pre', function () {
-        pre = +new Date();
-        stall(pre);
-      });
-      three.on('update', function () {
-        update = +new Date();
-        stall(update);
-      });
-      three.on('render', function () {
-        render = +new Date();
-        stall(render);
-      });
-      three.on('post', function () {
-        post = +new Date();
-      });
-
-      three.init();
+    three.on('pre', function () {
+      pre = count;
+      count += 1;
+    });
+    three.on('update', function () {
+      update = count;
+      count += 1;
+    });
+    three.on('render', function () {
+      render = count;
+      count += 1;
+    });
+    three.on('post', function () {
+      post = count;
+      count += 1;
     });
 
-    waitsFor(function() {
-      return pre > 0;
-    }, "The pre event should be called", 100);
+    three.init();
 
-    waitsFor(function() {
-      return update > 0;
-    }, "The update event should be called", 100);
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
-    waitsFor(function() {
-      return render > 0;
-    }, "The render event should be called", 100);
+    expect(pre).toBeGreaterThan(0);
+    expect(update).toBeGreaterThan(0);
+    expect(render).toBeGreaterThan(0);
+    expect(post).toBeGreaterThan(0);
 
-    waitsFor(function() {
-      return post > 0;
-    }, "The post event should be called", 100);
+    expect(update).toBeGreaterThan(pre);
+    expect(render).toBeGreaterThan(update);
+    expect(post).toBeGreaterThan(render);
 
-    runs(function () {
-      expect(pre).toBeGreaterThan(0);
-      expect(update).toBeGreaterThan(0);
-      expect(render).toBeGreaterThan(0);
-      expect(post).toBeGreaterThan(0);
-
-      expect(update).toBeGreaterThan(pre);
-      expect(render).toBeGreaterThan(update);
-      expect(post).toBeGreaterThan(render);
-
-      three.destroy();
-    });
-
+    three.destroy();
   });
-
 });
